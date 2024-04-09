d2c21663a87a178616bd666e996a921cf6d959b2
Juan Gilberto Romero Cortes <juan.romeroc@infotec.mx>
2024-04-09 13:29:28 -0600

    first commit

---
+++ src/test/java/mx/gob/senasica/arquetipo/ms/security/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.senasica.arquetipo.ms.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
